20e934c1e1bad933c9e635a7506679368172f6bd
noface <noface@outlook.com>
2012-12-17 21:48:26 +0100

    Use XPath for identification and do proper event handling per replaced node.

--- greentext.user.js
+++ greentext.user.js
@@ -10,48 +10,24 @@
 // ==/UserScript==
 
 (function() {
-  var TEXT_NODE, cdm_listener, changeTextNodes, d, green, init, insertion_listener, replaceTextContent, replacingContent;
+  var cdm_listener, changeTextNodes, css, d, init, insertion_listener;
 
   d = document;
 
-  TEXT_NODE = Node.TEXT_NODE || 3;
-
-  replacingContent = false;
-
-  green = "rgb(120, 153, 34)";
-
-  replaceTextContent = function(node) {
-    var span;
-    if (node.parentNode.getAttribute("contenteditable")) {
-      return;
-    }
-    replacingContent = true;
-    if (node.textContent.trim().substr(0, 1) === ">") {
-      span = d.createElement("span");
-      span.className = "implied";
-      span.textContent = node.textContent;
-      node.parentNode.replaceChild(span, node);
-    }
-    return replacingContent = false;
-  };
+  css = ".implied { color: rgb(120, 153, 34) }";
 
   changeTextNodes = function(node) {
-    var parent, _i, _len, _ref;
-    parent = node.parentNode;
-    if (parent.getAttribute("contenteditable") || (parent.className === "implied") || parent.style.color) {
-      return;
-    }
-    if (node.nodeType === TEXT_NODE) {
-      try {
-        return replaceTextContent(node);
-      } catch (err) {
-        return console.log(err);
-      }
-    } else {
-      _ref = node.childNodes;
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        node = _ref[_i];
-        changeTextNodes(node);
+    var data, i, snapshot, span, _i, _ref;
+    snapshot = d.evaluate('.//text()', node, null, 6, null);
+    for (i = _i = 0, _ref = snapshot.snapshotLength; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      node = snapshot.snapshotItem(i);
+      data = node.data;
+      if (data.trim().substr(0, 1) === ">") {
+        span = d.createElement("span");
+        span.className = "implied";
+        span.textContent = node.textContent;
+        span.addEventListener("DOMCharacterDataModified", cdm_listener, false);
+        node.parentNode.replaceChild(span, node);
       }
     }
   };
@@ -61,20 +37,17 @@
   };
 
   cdm_listener = function(event) {
-    if (!replacingContent) {
-      return replaceTextContent(event.target);
-    }
+    return event.target.parentNode.className = event.newValue.trim().substr(0, 1) === ">" ? "implied" : "unimplied";
   };
 
   init = function() {
     var style;
     style = d.createElement("style");
     style.type = "text/css";
-    style.textContent = ".implied { color: " + green + " }";
+    style.textContent = css;
     d.head.appendChild(style, d.head);
     changeTextNodes(d.body);
-    d.body.addEventListener("DOMNodeInserted", insertion_listener, false);
-    return d.body.addEventListener("DOMCharacterDataModified", cdm_listener, false);
+    return d.body.addEventListener("DOMNodeInserted", insertion_listener, false);
   };
 
   init();
