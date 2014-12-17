f6d17d17af6da5925589484238d75a5fc68d33c3
Brandon <turk.brandon@gmail.com>
2014-12-17 12:13:02 -0800

    update PixivUtil script

--- quickscript/PixivUtil.sh
+++ quickscript/PixivUtil.sh
@@ -1,21 +1,29 @@
-echo -e "Directory to download into: /Users/vampiricwulf/Pictures/4chi/\c "
+maindir="/Users/vampiricwulf/Pictures/4chi"
+echo -e "Directory to download into: $maindir/\c "
 read dir
-newdir="/Users/vampiricwulf/Pictures/4chi/$dir"
+newdir="$maindir/$dir"
 if [ -d "$newdir" ]; then
-    cd "$newdir"
+  cd "$newdir"
 else
-    read -p "Invalid Directory. Would you like to make this directory? " -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        cd "/Users/vampiricwulf/Pictures/4chi" 
-        mkdir -p "$dir"
-        cd "./$dir"
-    else
-        cd "/Users/vampiricwulf/Pictures/4chi"
-    fi
+  read -p "Invalid Directory. Would you like to make this directory? [Y/n] " -n 1 -r
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    cd "$maindir"
+    mkdir -p "$dir"
+  else
     echo
+    read -p "Would you like to continue? Directory defaults to: $maindir [Y/n] " -n 1 -r
+    if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+      echo
+      echo "Please close this Terminal window."
+      exit 0
+    fi
+    newdir="$maindir"
+  fi
+  cd "$newdir"
+  echo
 fi
 echo "Directory set to: $newdir"
 echo "Loading PixivUtil2. Please Wait."
-python /Applications/PixivUtil/PixivUtil2.py 
-echo "Please close this Terminal window as a typing bug tends to occur after PixivUtil exits."
+python /Applications/PixivUtil/PixivUtil2.py
+echo "Please close this Terminal window."
 exit 0
